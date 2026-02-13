c33cf4a4276b9b59bfbe1cb6b9175bb4c5d2e2f2
ISHITA172004 <dasishita614@gmail.com>
2026-02-13 20:55:15 +0530

    Fixed sklearn issue and updated recommender logic

--- app.py
+++ app.py
@@ -3,58 +3,92 @@ import pickle
 import pandas as pd
 import requests
 
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+
+# -------------------------------
+# Fetch Movie Poster from TMDB API
+# -------------------------------
 def fetch_poster(movie_id):
-    response=requests.get(
-    "https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(movie_id))
-
-    data=response.json()
-
+    response = requests.get(
+        f"https://api.themoviedb.org/3/movie/{movie_id}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US"
+    )
+    data = response.json()
     return "https://image.tmdb.org/t/p/w500/" + data['poster_path']
 
+
+# -------------------------------
+# Recommend Function
+# -------------------------------
 def recommend(movie):
     movie_index = movies[movies['title'] == movie].index[0]
     distances = similarity[movie_index]
-    movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
 
-    recommended_movies=[]
-    recommended_movies_posters=[]
+    movies_list = sorted(
+        list(enumerate(distances)),
+        reverse=True,
+        key=lambda x: x[1]
+    )[1:6]
+
+    recommended_movies = []
+    recommended_movies_posters = []
 
     for i in movies_list:
-        movie_id=movies.iloc[i[0]].movie_id
+        movie_id = movies.iloc[i[0]].movie_id
         recommended_movies.append(movies.iloc[i[0]].title)
-        # Fetch poster from API
         recommended_movies_posters.append(fetch_poster(movie_id))
-    return recommended_movies,recommended_movies_posters
+
+    return recommended_movies, recommended_movies_posters
 
 
-movies_dict=pickle.load(open('movie_dict.pkl','rb'))
-movies=pd.DataFrame(movies_dict)
+# -------------------------------
+# Load Movie Data
+# -------------------------------
+movies_dict = pickle.load(open('movie_dict.pkl', 'rb'))
+movies = pd.DataFrame(movies_dict)
 
-similarity=pickle.load(open('similarity.pkl','rb'))
 
-st.title('Movie Recommender System')
+# -------------------------------
+# Create Vectors (NO similarity.pkl)
+# -------------------------------
+cv = CountVectorizer(max_features=5000, stop_words='english')
+vectors = cv.fit_transform(movies['tags']).toarray()
 
-selected_movie_name=st.selectbox(
-"How would you like to be contacted?",
-movies['title'].values
+similarity = cosine_similarity(vectors)
+
+
+# -------------------------------
+# Streamlit UI
+# -------------------------------
+st.title('🎬 Movie Recommender System')
+
+selected_movie_name = st.selectbox(
+    "Select a movie",
+    movies['title'].values
 )
 
 if st.button('Recommend'):
-    recommended_movie_names,recommended_movie_posters=recommend(selected_movie_name)
+    names, posters = recommend(selected_movie_name)
+
     col1, col2, col3, col4, col5 = st.columns(5)
+
     with col1:
-        st.text(recommended_movie_names[0])
-        st.image(recommended_movie_posters[0])
+        st.text(names[0])
+        st.image(posters[0])
+
     with col2:
-        st.text(recommended_movie_names[1])
-        st.image(recommended_movie_posters[1])
+        st.text(names[1])
+        st.image(posters[1])
 
     with col3:
-        st.text(recommended_movie_names[2])
-        st.image(recommended_movie_posters[2])
+        st.text(names[2])
+        st.image(posters[2])
+
     with col4:
-        st.text(recommended_movie_names[3])
-        st.image(recommended_movie_posters[3])
+        st.text(names[3])
+        st.image(posters[3])
+
     with col5:
-        st.text(recommended_movie_names[4])
-        st.image(recommended_movie_posters[4])
+        st.text(names[4])
+        st.image(posters[4])
